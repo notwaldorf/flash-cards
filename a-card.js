ec59b72e604151dd4909a89c6293260f84b00b34
Monica Dinculescu <noms@google.com>
2018-01-28 22:28:13 -0800

    how do paths work even

--- a-card.js
+++ a-card.js
@@ -1,4 +1,4 @@
-import { LitElement, html } from '../node_modules/@polymer/lit-element/lit-element.js'
+import { LitElement, html } from './node_modules/@polymer/lit-element/lit-element.js'
 
 class ACard extends LitElement {
   render(props) {
