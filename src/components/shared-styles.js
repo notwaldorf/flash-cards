95e9d96332f17556888d208d681476073d82237e
Monica Dinculescu <noms@google.com>
2018-02-21 11:35:55 -0800

    always forget to check in new files

---
+++ src/components/shared-styles.js
@@ -0,0 +1,24 @@
+/**
+@license
+Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+Code distributed by Google as part of the polymer project is also
+subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+*/
+
+export const SharedStyles = `
+  :host {
+    display: block;
+    box-sizing: border-box;
+    margin: 0 auto;
+    padding: 40px;
+    max-width: 600px;
+  }
+  a:link, a:visited {
+    color: var(--app-header-selected-color);
+    text-decoration: none;
+    font-weight: bold;
+  }
+`;
